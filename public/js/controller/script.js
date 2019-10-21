cb7afbe3241f8a27a7bc5418076283234216cfb7
Geek2017 <jmlgrefaldo@bicol-u.edu.ph>
2019-10-21 23:00:09 +0800

    login

--- public/js/controller/script.js
+++ public/js/controller/script.js
@@ -94,6 +94,21 @@ $(document).ready(function () {
         });
       });
 
+      var email = document.querySelector('#resetloginEmail').value;
+             
+                
+
+      $("#resetEmail").on("submit", function (e) {
+              e.preventDefault();
+
+              const email = $('#resetloginEmail').val();
+              console.log(email);
+              
+              firebase.auth().sendPasswordResetEmail(email)
+              .then(function() {
+                  alert('Reset link has been sent to provided email address');
+              })
+          });
 
       $('#wizard').on('click', function(e) {
         window.location.replace("wizard.html")
